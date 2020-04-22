b4b3debaea3a2e4df35ce7bd1b6bc5a0a8e56306
alxnkt <49128174+alxnkt@users.noreply.github.com>
2020-04-22 21:20:35 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,3 +1,10 @@
-export default [
+const connections = {
+  mongo: 'mongodb://localhost/dbname',
+  postgres: 'postgres://postgres:secret@localhost:5432/dbname'
+};
+
+const collections = [
   // Define your database migration settings here
 ];
+
+export { connections, collections };
